1794e19c12262c27c5dbb369bbb80a96f513fa2f
Daniel Borowski <dborowski27@gmail.com>
2018-11-09 21:09:52 -0500

    init

---
+++ language/javascript/javascript.go
@@ -0,0 +1,11 @@
+package javascript
+
+import (
+	"github.com/danielborowski/cb-code-runner-2/cmd"
+	"path/filepath"
+)
+
+func Run(files []string, stdin string) (string, string, error) {
+	workDir := filepath.Dir(files[0])
+	return cmd.RunStdin(workDir, stdin, "node", files[0])
+}
---
+++ language/language.go
@@ -0,0 +1,20 @@
+package language
+
+import (
+	"github.com/danielborowski/cb-code-runner-2/language/javascript"
+)
+
+type runFn func([]string, string) (string, string, error)
+
+var languages = map[string]runFn{
+	"javascript":   javascript.Run,
+}
+
+func IsSupported(lang string) bool {
+	_, supported := languages[lang]
+	return supported
+}
+
+func Run(lang string, files []string, stdin string) (string, string, error) {
+	return languages[lang](files, stdin)
+}
