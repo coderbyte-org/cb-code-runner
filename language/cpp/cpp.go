dd770a92bf758521021ef4c2fab49760a05b5af2
Daniel Borowski <dborowski27@gmail.com>
2020-12-23 10:11:53 -0500

    update c++

--- language/cpp/cpp.go
+++ language/cpp/cpp.go
@@ -11,7 +11,7 @@ func Run(files []string, stdin string) (string, string, error) {
 	binName := "a.out"
 
 	sourceFiles := util.FilterByExtension(files, "cpp")
-	args := append([]string{"clang++", "-std=c++11", "-o", binName}, sourceFiles...)
+	args := append([]string{"clang++", "-std=c++20", "-o", binName}, sourceFiles...)
 	stdout, stderr, err := cmd.Run(workDir, args...)
 	if err != nil || stderr != "" {
 		return stdout, stderr, err
