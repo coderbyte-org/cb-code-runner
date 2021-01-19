58eceb568a3b4ceaad1d1eb54c5c644f3ff492f9
Daniel Borowski <dborowski27@gmail.com>
2021-01-19 11:07:28 -0500

    update kotlin

--- language/kotlin/kotlin.go
+++ language/kotlin/kotlin.go
@@ -10,7 +10,7 @@ func Run(files []string, stdin string) (string, string, error) {
 	workDir := filepath.Dir(files[0])
 	fname := filepath.Base(files[0])
 
-	stdout, stderr, err := cmd.Run(workDir, "kotlinc", fname)
+	stdout, stderr, err := cmd.Run(workDir, "kotlinc", fname, "-no-jdk", "-progressive", "-no-reflect")
 
 	// remove java warning
 	stderr = strings.Replace(stderr, "Java HotSpot(TM) 64-Bit Server VM warning: Options -Xverify:none and -noverify were deprecated in JDK 13 and will likely be removed in a future release.\n", "", -1)
