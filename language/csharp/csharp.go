bf5d66f0b76c1d6318bd136aed3276365629cba0
Daniel Borowski <dborowski27@gmail.com>
2021-04-20 17:25:51 -0400

    todo for C#

--- language/csharp/csharp.go
+++ language/csharp/csharp.go
@@ -10,6 +10,9 @@ func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
 	binName := "a.exe"
 
+	// TODO: getting JSON package to work with C#
+	// https://stackoverflow.com/questions/50079804/how-do-i-add-json-assembly-to-compile-c-sharp-code-on-ubuntu-terminal-using-mcs
+	// https://stackoverflow.com/questions/49767212/how-to-compile-and-run-c-sharp-project-from-terminal-mac-os
 	sourceFiles := util.FilterByExtension(files, "cs")
 	args := append([]string{"mcs", "-out:" + binName}, sourceFiles...)
 	stdout, stderr, err, duration := cmd.Run(workDir, args...)
