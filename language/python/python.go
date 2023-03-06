fdb2e8bafef181a0716a4ff6c982f17dbaa971c1
Daniel Borowski <dborowski27@gmail.com>
2023-03-05 20:58:30 -0500

    pyspark errors

--- language/python/python.go
+++ language/python/python.go
@@ -7,5 +7,11 @@ import (
 
 func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
-	return cmd.RunStdin(workDir, stdin, "python", files[0])
+	stdout, stderr, err, duration = cmd.RunStdin(workDir, stdin, "python", files[0])
+
+	// remove pyspark warnings
+	stdout = strings.Replace(stdout, "WARN NativeCodeLoader: Unable to load native-hadoop library for your platform... using builtin-java classes where applicable\n", "", -1)
+	stderr = strings.Replace(stderr, "Setting default log level to \"WARN\".\nTo adjust logging level use sc.setLogLevel(newLevel). For SparkR, use setLogLevel(newLevel).\n", "", -1)
+
+	return stdout, stderr, err, duration
 }
