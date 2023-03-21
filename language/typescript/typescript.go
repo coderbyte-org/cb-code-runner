bfe0fd76881c1540d1a9fd4f2f38dce26657fdeb
Daniel Borowski <dborowski27@gmail.com>
2023-03-21 15:18:22 -0400

    ts changes for modules

--- language/typescript/typescript.go
+++ language/typescript/typescript.go
@@ -8,11 +8,12 @@ import (
 
 func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
-	jsName := "a.js"
+	jsName := "main.js"
 
 	// Find all typescript files and build compile command
+	// file sent to coderunner is always `main.ts` so it compiles to `main.js`
 	sourceFiles := util.FilterByExtension(files, "ts")
-	args := append([]string{"tsc", "-incremental", "-skipLibCheck", "-target", "esnext", "-declaration", "-module", "system", "-outFile", jsName}, sourceFiles...)
+	args := append([]string{"tsc", "-skipLibCheck", "-target", "esnext", "-declaration", "-module", "commonjs"}, sourceFiles...)
 
 	// Compile to javascript
 	stdout, stderr, err, duration := cmd.Run(workDir, args...)
