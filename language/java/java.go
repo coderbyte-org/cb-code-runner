9fe4e0e34d7c62701067d988d45f3f519a69eb88
Daniel Borowski <dborowski27@gmail.com>
2023-04-30 13:54:28 -0400

    add junit to java

--- language/java/java.go
+++ language/java/java.go
@@ -9,12 +9,15 @@ func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
 	fname := filepath.Base(files[0])
 
-	stdout, stderr, err, duration := cmd.Run(workDir, "javac", "-Xlint:unchecked", fname)
+	// need to include junit into classpath env variable
+	junitIncludeString := ".:/usr/local/bin/JUNIT/junit-4.13.2.jar:/usr/local/bin/JUNIT/hamcrest-core-1.3.jar"
+	stdout, stderr, err, duration := cmd.Run(workDir, "javac", "-Xlint:unchecked", "-cp", junitIncludeString, fname)
+		
 	if err != nil || stderr != "" {
 		return stdout, stderr, err, duration
 	}
 
-	return cmd.RunStdin(workDir, stdin, "java", className(fname))
+	return cmd.RunStdin(workDir, stdin, "java", "-cp", junitIncludeString, className(fname))
 }
 
 func className(fname string) string {
