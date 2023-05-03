2d7d475f901e32ac1a210a861bdba7a08e4f52f4
Daniel Borowski <dborowski27@gmail.com>
2023-05-03 14:42:07 -0400

    csharp updates

--- language/csharp/csharp.go
+++ language/csharp/csharp.go
@@ -2,34 +2,22 @@ package csharp
 
 import (
 	"github.com/coderbyte-org/cb-code-runner/cmd"
-	"github.com/coderbyte-org/cb-code-runner/util"
 	"path/filepath"
-	"io"
-	"os"
 )
 
 func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
-	binName := "a.exe"
-	sourceFiles := util.FilterByExtension(files, "cs")
+	projFile := "Main.csproj"
 
-	// for C#, need to link .dll files to this project and include them in directory
-	// https://stackoverflow.com/questions/50079804/how-do-i-add-json-assembly-to-compile-c-sharp-code-on-ubuntu-terminal-using-mcs
-	// https://stackoverflow.com/questions/49767212/how-to-compile-and-run-c-sharp-project-from-terminal-mac-os
-	// https://docs.unity3d.com/462/Documentation/Manual/UsingDLL.html
-	oldLocation := "/usr/local/bin/Newtonsoft.Json.dll"
-	newLocation := workDir + "/Newtonsoft.Json.dll"
-	srcFile, err := os.Open(oldLocation)
-	destFile, err := os.Create(newLocation)
-	_, err = io.Copy(destFile, srcFile)
-	
-	args := append([]string{"mcs", "-r:Newtonsoft.Json.dll", "-out:" + binName}, sourceFiles...)
+	// run using `dotnet` now instead of the previous `mcs` method, this allows us to add .csproj files
+	// https://stackoverflow.com/a/64646610
+	args := append([]string{"dotnet", "build"}, projFile)
 
 	stdout, stderr, err, duration := cmd.Run(workDir, args...)
 	if err != nil || stderr != "" {
 		return stdout, stderr, err, duration
 	}
 
-	binPath := filepath.Join(workDir, binName)
-	return cmd.RunStdin(workDir, stdin, "mono", binPath)
+	binPath := filepath.Join(workDir, projFile)
+	return cmd.RunStdin(workDir, stdin, "dotnet", "run", binPath)
 }
