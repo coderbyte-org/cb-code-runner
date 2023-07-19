67604e4bdbdf54fc3b7afbd1c7f2ee73539f16a5
Daniel Borowski <dborowski27@gmail.com>
2023-07-18 22:20:35 -0400

    update java jar

--- language/java/java.go
+++ language/java/java.go
@@ -9,16 +9,16 @@ func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
 	fname := filepath.Base(files[0])
 
-	// need to include junit into classpath env variable
+	// need to include junit into classpath env variable along with any other .jar files
 	// https://github.com/junit-team/junit4/wiki/Getting-started
-	junitIncludeString := ".:/usr/local/bin/JUNIT/junit-4.13.2.jar:/usr/local/bin/JUNIT/hamcrest-core-1.3.jar"
-	stdout, stderr, err, duration := cmd.Run(workDir, "javac", "-Xlint:unchecked", "-cp", junitIncludeString, fname)
+	jarFileIncludes := ".:/usr/local/bin/JUNIT/junit-4.13.2.jar:/usr/local/bin/JUNIT/hamcrest-core-1.3.jar:/usr/local/bin/JUNIT/gson-2.10.1.jar"
+	stdout, stderr, err, duration := cmd.Run(workDir, "javac", "-Xlint:unchecked", "-cp", jarFileIncludes, fname)
 		
 	if err != nil || stderr != "" {
 		return stdout, stderr, err, duration
 	}
 
-	return cmd.RunStdin(workDir, stdin, "java", "-cp", junitIncludeString, className(fname))
+	return cmd.RunStdin(workDir, stdin, "java", "-cp", jarFileIncludes, className(fname))
 }
 
 func className(fname string) string {
