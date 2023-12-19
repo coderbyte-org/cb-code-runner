f00e53de6cefc5f070656195e5858fe67adced33
Daniel Borowski <dborowski27@gmail.com>
2023-12-19 11:25:04 -0500

    kotlin jar update

--- language/kotlin/kotlin.go
+++ language/kotlin/kotlin.go
@@ -10,23 +10,24 @@ func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
 	fname := filepath.Base(files[0])
 
+	// need to include junit into classpath env variable along with any other .jar files
+	// https://github.com/junit-team/junit4/wiki/Getting-started
+	jarFileIncludes := "/usr/local/bin/JUNIT/junit-4.13.2.jar:/usr/local/bin/JUNIT/hamcrest-core-1.3.jar:/usr/local/bin/JUNIT/gson-2.10.1.jar:/usr/local/bin/JUNIT/okio-1.9.0.jar:/usr/local/bin/JUNIT/okhttp-3.9.1.jar:/usr/local/bin/JUNIT/approvaltests-4.1.jar:."
+
 	// files we need to compile
 	// https://discuss.kotlinlang.org/t/kotlinc-example-with-separate-files/3389/4
-	compileCommand := []string{"kotlinc", "-progressive", "-no-reflect"}
+	compileCommand := []string{"kotlinc", "-progressive", "-no-reflect", "-cp", jarFileIncludes}
 	for i := 0; i < len(files); i++ {
 		compileCommand = append(compileCommand, strings.ReplaceAll(files[i], workDir + "/", ""))
 	}
 
 	stdout, stderr, err, duration := cmd.Run(workDir, compileCommand...)
 
-	// remove java warning
-	stderr = strings.Replace(stderr, "Java HotSpot(TM) 64-Bit Server VM warning: Options -Xverify:none and -noverify were deprecated in JDK 13 and will likely be removed in a future release.\n", "", -1)
-
 	if err != nil || stderr != "" {
 		return stdout, stderr, err, duration
 	}
 
-	return cmd.RunStdin(workDir, stdin, "kotlin", className(fname))
+	return cmd.RunStdin(workDir, stdin, "kotlin", "-cp", jarFileIncludes, className(fname))
 }
 
 func className(fname string) string {
