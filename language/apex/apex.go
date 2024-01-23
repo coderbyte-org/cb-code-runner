f7003e3831e7bdf19c7072ed18c33886c74a6163
Daniel Borowski <dborowski27@gmail.com>
2024-01-22 21:54:39 -0500

    support for apex

--- language/apex/apex.go
+++ language/apex/apex.go
@@ -9,5 +9,5 @@ func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
 
 	// https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_develop_apex_run_anon.htm
-	return cmd.RunStdin(workDir, stdin, "sudo", "sf", "apex", "run", "--file", files[0], "--target-org", "my-hub-org")
+	return cmd.RunStdin(workDir, stdin, "sf", "apex", "run", "--file", files[0], "--target-org", "my-hub-org")
 }
