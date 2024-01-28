b931f0adea4b4a5877e65deebdc75eb27c8748d5
Daniel Borowski <dborowski27@gmail.com>
2024-01-28 14:29:23 -0500

    update dart runner

--- language/dart/dart.go
+++ language/dart/dart.go
@@ -7,5 +7,14 @@ import (
 
 func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
+
+	// first install based on .yaml file
+	stdout, stderr, err, duration := cmd.Run(workDir, "dart", "pub", "get")
+		
+	if err != nil || stderr != "" {
+		return stdout, stderr, err, duration
+	}
+
+	// run dart program
 	return cmd.RunStdin(workDir, stdin, "dart", "run", files[0])
 }
