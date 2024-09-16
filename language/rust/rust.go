5cba63948d3f7f0cb923509ab7beeba9fac391ff
Daniel Borowski <dborowski27@gmail.com>
2024-09-15 22:47:11 -0400

    rust tests

--- language/rust/rust.go
+++ language/rust/rust.go
@@ -3,17 +3,36 @@ package rust
 import (
 	"github.com/coderbyte-org/cb-code-runner/cmd"
 	"path/filepath"
+	"strings"
 )
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+			if strings.Contains(a, e) {
+					return true
+			}
+	}
+	return false
+}
 
 func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
 	binName := "a.out"
+	containsToml := contains(files, "Cargo.toml")
 
-	stdout, stderr, err, duration := cmd.Run(workDir, "rustc", "-o", binName, files[0])
-	if err != nil {
-		return stdout, stderr, err, duration
+	// if contains toml, we run unit tests
+	if (containsToml) {
+		stdout, stderr, err, duration := cmd.Run(workDir, "cargo", "build")
+		if err != nil {
+			return stdout, stderr, err, duration
+		}
+		return cmd.RunStdin(workDir, stdin, "cargo", "test")
+	} else {
+		stdout, stderr, err, duration := cmd.Run(workDir, "rustc", "-o", binName, files[0])
+		if err != nil {
+			return stdout, stderr, err, duration
+		}
+		binPath := filepath.Join(workDir, binName)
+		return cmd.RunStdin(workDir, stdin, binPath)
 	}
-
-	binPath := filepath.Join(workDir, binName)
-	return cmd.RunStdin(workDir, stdin, binPath)
 }
