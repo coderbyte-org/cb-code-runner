729a17fd05355733c2893e17948386c074900c85
Daniel Borowski <dborowski27@gmail.com>
2025-11-13 11:10:48 -0500

    allow custom config file

--- language/python/python.go
+++ language/python/python.go
@@ -1,11 +1,18 @@
 package python
 
 import (
+	"github.com/coderbyte-org/cb-code-runner/config"
 	"github.com/coderbyte-org/cb-code-runner/cmd"
 	"path/filepath"
 )
 
 func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
-	return cmd.RunStdin(workDir, stdin, "python", files[0])
+	runArgs := []string{"python", files[0]}
+
+	if cfgArgs := config.ParseCbConfig(files); cfgArgs != nil {
+		runArgs = cfgArgs
+	}
+
+	return cmd.RunStdin(workDir, stdin, runArgs...)
 }
