cc19f31718282718511b9542802017fb5c260914
Daniel Borowski <dborowski27@gmail.com>
2025-12-16 11:55:01 -0500

    go update

--- language/golang/golang.go
+++ language/golang/golang.go
@@ -1,11 +1,18 @@
 package golang
 
 import (
+	"github.com/coderbyte-org/cb-code-runner/config"
 	"github.com/coderbyte-org/cb-code-runner/cmd"
 	"path/filepath"
 )
 
 func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
-	return cmd.RunStdin(workDir, stdin, "go", "run", ".")
+	runArgs := []string{"go", "run", "."}
+
+	if cfgArgs := config.ParseCbConfigField(files, "run"); cfgArgs != nil {
+		runArgs = cfgArgs
+	}
+
+	return cmd.RunStdin(workDir, stdin, runArgs...)
 }
