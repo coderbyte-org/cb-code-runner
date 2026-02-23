4d8b190acff46e55aac17dc738d4a38949fd3405
Daniel Borowski <dborowski27@gmail.com>
2026-02-23 11:59:57 -0500

    apex run update

--- language/apex/apex.go
+++ language/apex/apex.go
@@ -1,23 +1,65 @@
 package apex
 
 import (
-	"github.com/coderbyte-org/cb-code-runner/cmd"
-	"path/filepath"
 	"os"
+	"path/filepath"
+	"github.com/coderbyte-org/cb-code-runner/cmd"
+	"github.com/coderbyte-org/cb-code-runner/config"
+	"github.com/coderbyte-org/cb-code-runner/util"
 )
 
 func Run(files []string, stdin string) (string, string, error, string) {
 	workDir := filepath.Dir(files[0])
 
+	// Collect possible Apex source files for ENV_SOURCE_FILES
+	sourceFiles := append([]string{},
+		util.FilterByExtension(files, "apex")...,
+	)
+	sourceFiles = append(sourceFiles, util.FilterByExtension(files, "cls")...)
+	sourceFiles = append(sourceFiles, util.FilterByExtension(files, "trigger")...)
+
 	// need to auth first
 	// https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_auth_jwt_flow.htm
-	stdout, stderr, err, duration := cmd.Run(workDir, "sf", "org", "login", "jwt", "--client-id", os.Getenv("CONSUMER_ID_SALESFORCE"), "--jwt-key-file", "/usr/local/bin/JWT/server.key", "--username", "support@coderbyte.com", "--alias", "my-hub-org", "--set-default-dev-hub")
-		
-	if err != nil {
-		return stdout, stderr, err, duration
+	authArgs := []string{
+		"sf", "org", "login", "jwt",
+		"--client-id", os.Getenv("CONSUMER_ID_SALESFORCE"),
+		"--jwt-key-file", "/usr/local/bin/JWT/server.key",
+		"--username", "support@coderbyte.com",
+		"--alias", "my-hub-org",
+		"--set-default-dev-hub",
 	}
 
-	// run apex code
-	// https://developer.salesforce.com/docs/atlas.en-us.sfdx_dev.meta/sfdx_dev/sfdx_dev_develop_apex_run_anon.htm
-	return cmd.RunStdin(workDir, stdin, "sf", "apex", "run", "--file", files[0], "--target-org", "my-hub-org")
+	authStdout, authStderr, authErr, authDuration := cmd.Run(workDir, authArgs...)
+	if authErr != nil {
+		return authStdout, authStderr, authErr, authDuration
+	}
+
+	// default run command
+	runArgs := []string{
+		"sf", "apex", "run",
+		"--file", files[0],
+		"--target-org", "my-hub-org",
+	}
+
+	// Allow .cbconfig override of "run"
+	if cfgRun := config.ParseCbConfigField(files, "run"); cfgRun != nil {
+		var expanded []string
+		for _, part := range cfgRun {
+			switch part {
+			case "ENV_SOURCE_FILES":
+				expanded = append(expanded, sourceFiles...)
+			default:
+				expanded = append(expanded, part)
+			}
+		}
+		runArgs = expanded
+	}
+
+	runStdout, runStderr, runErr, runDuration := cmd.RunStdin(workDir, stdin, runArgs...)
+
+	// Combine auth + run logs
+	combinedStdout := authStdout + runStdout
+	combinedStderr := authStderr + runStderr
+
+	return combinedStdout, combinedStderr, runErr, runDuration
 }
